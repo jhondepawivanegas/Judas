9ab5e91fee5e48972d0eb2898c441218e992bb52
jhondepawivanegas <jhondepawi@gmail.com>
2023-10-02 09:58:14 -0500

    cambios

--- js/carrito.js
+++ js/carrito.js
@@ -1,26 +1,43 @@
-let numSumar = document.getElementById("numSumar");
-let numRestar = document.getElementById("numRestar");
-let numView = document.getElementById("numView");
+let count = document.getElementById("count")
+let subTotal = document.getElementById("subTotal")
+let cantProductos = document.getElementById("cantProducts")
+let deleteButton = document.getElementById("delete")
+let btn3 = document.getElementById("btn-3")
+let btn1 = document.getElementById("btn-1")
+let numProduct = document.getElementById("numProduct")
 
+let num = 0;
+let valor = 5000;
+btn3.addEventListener("click", function() {
+    num++;
+    count.textContent = num;
+    subTotal.textContent = valor * num;
+    cantProductos.textContent = num;
 
-numRestar.addEventListener("click", function(){
-    let aux = numView.innerHTML.toString();
-    let auxInt =parseInt(aux);
-    if(auxInt>0){
-        auxInt--;
-        document.getElementById("numView").innerText = auxInt.toString();
+    if (num == 1) {
+        numProduct.textContent = "producto";
+    } else if (num > 1) {
+        numProduct.textContent = "productos"
     }
    
 })
 
-numSumar.addEventListener("click", function(){
-    let aux = numView.innerHTML.toString();
-    let auxInt =parseInt(aux);
-    if(auxInt<10){
-        auxInt++;
-        document.getElementById("numView").innerText = auxInt.toString();
-
+btn1.addEventListener("click", function() {
+    if (num > 0 ) {
+        num--;
+        count.textContent = num;
+        subTotal.textContent = valor * num;
+        cantProductos.textContent = num;
+    } else if (num <= 0) {
+        count.textContent = 0;
+        subTotal.textContent = 0;
+        numProduct.textContent = " ";
     }
-    
-   
 })
+deleteButton.addEventListener("click", function() {
+    num = 0;
+    count.textContent = 0;
+    subTotal.textContent = 0;
+    cantProductos.textContent = 0;
+    numProduct.textContent = " ";
+})  
