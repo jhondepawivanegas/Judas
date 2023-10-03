f68eea06d03492d0173d1b83ee20836d387508ab
jhondepawivanegas <jhondepawi@gmail.com>
2023-10-03 11:17:56 -0500

    cambios

---
+++ js/mercado.js
@@ -0,0 +1,13 @@
+
+let
+class Automovil{
+    constructor(marca,Modelo){
+        this.marca = marca;
+        this.Modelo = Modelo;
+
+    }
+}
+let auto1 = new Automovil("Mazda","Cx-30");
+alert (auto1.marca + "" +   auto1.Modelo);
+
+
