98a2de8b675c689719b203f33bf16de84946275a
jhondepawivanegas <jhondepawi@gmail.com>
2023-10-17 11:39:40 -0500

    cambios

--- js/merca.js
+++ js/merca.js
@@ -1,43 +1,92 @@
-/* Cuando trabajamos con objetos, debo tener en cuenta
-que un objeto se construye teniendo en cuenta una clase */
-let inputBusqueda = document.getElementById("inputBusqueda");
-class Automovil {
-    constructor(marca, modelo) {
+/* cuando trabajamos con objetos debo tener en cuenta que en un objeto se construye teniendo encuenta una clase*/
+// el contructor crea la clase osea la estructura
+let inputBusqueda = document.getElementById
+("inputBusqueda");
+class Automovil{
+    constructor(marca,modelo,precio,imagen){
+
         this.marca = marca;
         this.modelo = modelo;
+        this.precio = precio;
+        this.imagen =imagen;
+
     }
 }
 
-let auto1 = new Automovil("Toyota", "TXL");
+let auto1= new Automovil("toyota", "TXL",15000000,"imagenes/prado.disel.webp");
 
-//alert(auto1.marca + " " + auto1.modelo);
-window.addEventListener("load", function(event){
-//inputBusqueda.addEventListener("keydown", function(event) {
-   // if (event.key == "Enter") {
-        let mainContent = document.getElementById("mainContent");
-        /* Caja para el producto */
-        let boxProducto = document.createElement("div");
-        mainContent.appendChild(boxProducto);
-        boxProducto.setAttribute("class", "box-producto");
+//alert(auto1.marca+ " "+auto1.modelo);
 
-        /* Caja para la imagen */
-        let boxImg = document.createElement("div");
-        boxProducto.appendChild(boxImg);
-        boxImg.setAttribute("class", "box-img");
-        let imgAuto = document.createElement("img");
-        boxImg.appendChild(imgAuto);
-        imgAuto.setAttribute("src", "imagenes/prado.disel.webp");
-        imgAuto.setAttribute("class", "img-auto");
 
-        /* Caja para la información */
-        let boxInfo = document.createElement("div");
-        boxProducto.appendChild(boxInfo);
-        boxInfo.setAttribute("class", "box-info");
-        let marca = document.createElement("label");
-        boxInfo.appendChild(marca);
-        let txtnodemarca = document.createTextNode(auto1.marca +""+ auto1.modelo);
-        marca.appendChild(txtnodemarca);
-   //}
+window.addEventListener("load",function(){
+    let mainContent=document.getElementById("mainContent");
+    let boxproducto=document.createElement("div");
+    mainContent.appendChild(boxproducto);
+    boxproducto.setAttribute("class", "box-producto");
+
+    
+    /*caja para la imagen*/
+    let boximg= document.createElement("div");
+    boxproducto.appendChild(boximg);
+    boximg.setAttribute("class", "box-img");
+    let imgAuto= document.createElement("img");
+    boximg.appendChild(imgAuto);
+    imgAuto.setAttribute("src", auto1.imagen);
+    imgAuto.setAttribute("class", "img-auto");      
+
+
+    
+    //caja información
+    let boxInfo = document.createElement("div");
+    boxproducto.appendChild(boxInfo);
+    boxInfo.setAttribute("class", "box-info");
+
+    let marcaL = document.createElement("label");
+    boxInfo.appendChild(marcaL);
+    let txtNodeMarca = document.createTextNode(auto1.marca + " " + auto1.modelo);
+    marcaL.appendChild(txtNodeMarca);
+    marcaL.setAttribute("class","marca");
+
+      
+    /*precio*/
+    let precio = document.createElement("label");
+    boxInfo.appendChild(precio);
+    
+
+    // Formatea el precio utilizando toLocaleString()
+    let precioFormateado = auto1.precio.toLocaleString();
+    
+    let txtNodePrecio = document.createTextNode("$" + precioFormateado);
+    precio.appendChild(txtNodePrecio);
+    precio.setAttribute("class", "precio");
+    
+
+
+
+    /* Información adicional */
+let infoAdicional = document.createElement("label");
+boxInfo.appendChild(infoAdicional);
+
+let infoText = document.createTextNode("2021 - 25.000 km,  pitalito - huila");
+infoAdicional.appendChild(infoText);
+infoAdicional.setAttribute("class", "info-adicional");
+
+/*icono corazon*/
+let boxCorazon=document.createElement("div");
+boximg.appendChild(boxCorazon)
+
+let iconoCorazon =document.createElement("i");
+boxCorazon.appendChild(iconoCorazon);
+
+boxCorazon.setAttribute("class","box-corazon");
+iconoCorazon.setAttribute("class", "icon-corazon fa-regular fa-heart");
+
+/*linea divisora*/
+
+let lineaDiv=document.createElement("hr");
+boxproducto.appendChild(lineaDiv);
+lineaDiv.setAttribute("class","linea-div");
+
+
+
 });
-
-
--- js/mercado.js
+++ js/mercado.js
@@ -1,44 +1,59 @@
 /* cuando trabajamos con objetos debo tener en cuenta que en un objeto se construye teniendo encuenta una clase*/
 // el contructor crea la clase osea la estructura
-
+let inputBusqueda = document.getElementById
+("inputBusqueda");
 class Automovil{
-    constructor(marca,modelo,edad){
+    constructor(marca,modelo,precio,imagen){
 
         this.marca = marca;
         this.modelo = modelo;
-        this.edad = edad;
+        this.precio = precio;
+        this.imagen =imagen;
 
     }
 }
 
-let auto1= new Automovil("toyota", "TXL");
+let auto1= new Automovil("toyota", "TXL",15000000,"img/prado.disel.webp");
 
-alert(auto1.marca+ " "+auto1.modelo);
+//alert(auto1.marca+ " "+auto1.modelo);
 
 
-inputBusqueda.addEvenListener("Keydown",function(){
+window.addEventListener("load",function(){
     let mainContent=document.getElementById("mainContent");
     let boxproducto=document.createElement("div");
     mainContent.appendChild(boxproducto);
     boxproducto.setAttribute("class", "box-producto");
 
-    /*caja para el producto*/
-    let boxProducto = document.createElement("div");
-
+    
     /*caja para la imagen*/
     let boximg= document.createElement("div");
     boxproducto.appendChild(boximg);
     boximg.setAttribute("class", "box-img");
     let imgAuto= document.createElement("img");
     boximg.appendChild(imgAuto);
-    imgAuto.setAttribute("src", img/prado.disel.webp);
-    imgAuto.setAttribute("class", "img-auto");
+    imgAuto.setAttribute("src", auto1.imagen);
+    imgAuto.setAttribute("class", "img-auto");      
 
 
-    /*caja para la informacion*/
+    
+    //caja información
     let boxInfo = document.createElement("div");
     boxproducto.appendChild(boxInfo);
     boxInfo.setAttribute("class", "box-info");
 
-});
+    let marcaL = document.createElement("label");
+    boxInfo.appendChild(marcaL);
+    let txtNodeMarca = document.createTextNode(auto1.marca + " " + auto1.modelo);
+    marcaL.appendChild(txtNodeMarca);
+    marcaL.setAttribute("class","marca");
 
+      
+    /*precio*/
+    let precio = document.createElement("label");
+    boxInfo.appendChild(precio);
+    
+    let TxtNodeprecio  = document.createTextNode("$"+auto1.precio);
+    precio.appendChild(TxtNodeprecio);
+    precio.setAttribute("class","precio");
+
+});
